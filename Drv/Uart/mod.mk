5a886e30d573fbf2976675d3f2cec6597642d198
M Starch <lestarch@googlemail.com>
2018-06-10 13:44:34 -0700

    lestarch: adding UART drv

---
+++ Drv/Uart/mod.mk
@@ -0,0 +1,15 @@
+# ---------------------------------------------------------------------- 
+# mod.mk
+# ---------------------------------------------------------------------- 
+
+SRC = \
+	UartComponentAi.xml \
+        UartComponentImpl.cpp
+
+HDR = \
+        UartComponentImpl.hpp
+
+SRC_LINUX = \
+        UartLinux.cpp
+
+#SUBDIRS = test
